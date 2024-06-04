e7fdb36e88696401fe141b0c62e63dbc8e094cd3
apacteclab <103569759+apacteclab@users.noreply.github.com>
2024-06-04 11:47:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import random
 import json
 import requests
 
-option_a = os.getenv('OPTION_A', u"CI CD (Tekton) Live Demo Test # 22222222222")
+option_a = os.getenv('OPTION_A', u"CI CD (Tekton) Live Demo Test # 444444444444444444444444444444")
 option_b = os.getenv('OPTION_B', u"" )
 hostname = socket.gethostname()
 
